baf0eca8ca857281dd25895cf8e0cc3520e04cf4
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2021-02-15 00:36:33 +0530

    Add files via upload

---
+++ Edge detection/edge_detection.py
@@ -0,0 +1,174 @@
+import cv2
+import imutils
+import numpy as np
+
+
+
+image = cv2.imread('/home/srinivas/Documents/new/im.jpeg')
+ratio = image.shape[0]/500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+cv2.imshow('edge_detection', edged)
+cv2.waitKey()
+
+#---------------------------------------------------------------------------------
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow('Canny Edge Detection',image)
+cv2.waitKey()
+
+#---------------------------------------------------------------------------------
+
+def order_points(pts):
+
+	rect = np.zeros((4, 2), dtype = "float32")
+
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+	return rect
+
+def four_point_transform(image, pts):
+	# obtain a consistent order of the points and unpack them
+	# individually
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	# compute the width of the new image, which will be the
+	# maximum distance between bottom-right and bottom-left
+	# x-coordiates or the top-right and top-left x-coordinates
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	# compute the height of the new image, which will be the
+	# maximum distance between the top-right and bottom-right
+	# y-coordinates or the top-left and bottom-left y-coordinates
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	# now that we have the dimensions of the new image, construct
+	# the set of destination points to obtain a "birds eye view",
+	# (i.e. top-down view) of the image, again specifying points
+	# in the top-left, top-right, bottom-right, and bottom-left
+	# order
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	# return the warped image
+	return warped
+
+from skimage.filters import threshold_local
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Transformed Image',imutils.resize(warped, height = 650))
+cv2.waitKey()
+
+#-----------------------------------------------------------------------------
+
+# import the necessary packages
+import numpy as np
+import imutils
+import cv2
+
+def align_images(image, template, maxFeatures=500, keepPercent=0.2,
+	debug=False):
+	# convert both the input image and template to grayscale
+	imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+	# use ORB to detect keypoints and extract (binary) local
+	# invariant features
+	orb = cv2.ORB_create(maxFeatures)
+	(kpsA, descsA) = orb.detectAndCompute(imageGray, None)
+	(kpsB, descsB) = orb.detectAndCompute(templateGray, None)
+
+	# match the features
+	method = cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
+	matcher = cv2.DescriptorMatcher_create(method)
+	matches = matcher.match(descsA, descsB, None)
+
+	# sort the matches by their distance (the smaller the distance,
+	# the "more similar" the features are)
+	matches = sorted(matches, key=lambda x:x.distance)
+
+	# keep only the top matches
+	keep = int(len(matches) * keepPercent)
+	matches = matches[:keep]
+
+	# check to see if we should visualize the matched keypoints
+	if debug:
+		matchedVis = cv2.drawMatches(image, kpsA, template, kpsB,
+			matches, None)
+		matchedVis = imutils.resize(matchedVis, width=1000)
+		cv2.imshow('Key features',matchedVis)
+		cv2.waitKey(0)
+
+	# allocate memory for the keypoints (x,y-coordinates) from the
+	# top matches -- we'll use these coordinates to compute our
+	# homography matrix
+	ptsA = np.zeros((len(matches), 2), dtype="float")
+	ptsB = np.zeros((len(matches), 2), dtype="float")
+
+	# loop over the top matches
+	for (i, m) in enumerate(matches):
+		# indicate that the two keypoints in the respective images
+		# map to each other
+		ptsA[i] = kpsA[m.queryIdx].pt
+		ptsB[i] = kpsB[m.trainIdx].pt
+
+	# compute the homography matrix between the two sets of matched
+	# points
+	(H, mask) = cv2.findHomography(ptsA, ptsB, method=cv2.RANSAC)
+
+	# use the homography matrix to align the images
+	(h, w) = template.shape[:2]
+	aligned = cv2.warpPerspective(image, H, (w, h))
+
+	# return the aligned image
+	return aligned
+
+#-------------------------------------------------------------------------------
+
+image = warped
+template = cv2.imread('/home/srinivas/Documents/new/template.png')
+aligned = align_images(image, template, debug=True)
+aligned = imutils.resize(aligned, width=700)
+template = imutils.resize(template, width=700)
+
+stacked = np.hstack([aligned, template])
+overlay = template.copy()
+output = aligned.copy()
+
+#-----------------------------------------------------------------------------
+
+cv2.imshow('final-transformed-Image',stacked)
+cv2.waitKey()
