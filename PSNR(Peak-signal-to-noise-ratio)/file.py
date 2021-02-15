260003587f0bfdbabf257d672be5209c44a691e6
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2021-02-15 21:11:20 +0530

    Add files via upload

---
+++ PSNR(Peak-signal-to-noise-ratio)/file.py
@@ -0,0 +1,25 @@
+import cv2
+original1 = ''
+compressed1 = ''
+
+
+img = cv2.imread(original1, cv2.IMREAD_UNCHANGED)
+width = 500
+height = 500
+dim = (width, height)
+original = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow('original',original)
+cv2.waitKey()
+
+
+compressed = cv2.imread(compressed1,1)
+width = 500
+height = 500
+dim = (width, height)
+compressed = cv2.resize(compressed , dim, interpolation = cv2.INTER_AREA)
+cv2.imshow(compressed) 
+cv2.waitKey()
+
+value = PSNR(original, compressed)
+print(f"PSNR value is {value} dB") 
+
